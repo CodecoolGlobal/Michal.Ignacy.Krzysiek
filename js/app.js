248971cfc6bc7bae101030bb246beba68219b9d4
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2020-05-12 14:43:14 +0200

    Add eventlisteners to buttons with functions

--- js/app.js
+++ js/app.js
@@ -13,6 +13,56 @@ document.querySelector('.btn-new').addEventListener('click', init);
 
 init();
 
+document.querySelector('.btn-roll').addEventListener('click', function () {
+
+    if (gamePlaying) {
+        //1.RANDOM NUMBER
+        var dice = Math.floor(Math.random() * 6) + 1;
+        //2.DISPLAY THE RESULT
+        const diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+        //3.UPDATE THE ROUND SCORE IF THE ROLLED NUMBER WAS NOT A 1
+        if(dice === 6 && previous === 6){
+            document.querySelector('#current-' + activePlayer).textContent = 0;
+            roundScore = 0;
+            nextPlayer();
+        } else if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            // document.querySelector('.dice').style.display = 'none';
+            nextPlayer();
+        }
+        previous = dice;
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    if (gamePlaying) {
+
+        //add current score to global score
+        scores[activePlayer] += roundScore;
+
+        //update the ui
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        var inputScore = 20;
+
+        //check if player won the game
+        if (scores[activePlayer] >= inputScore) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            //Next player
+            nextPlayer();
+        }
+    }
+});
+
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
