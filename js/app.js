c95ab59902de2d734adbbc09ed24b394f0186296
MikeZielony <51535787+MikeZielony@users.noreply.github.com>
2020-05-15 08:06:53 +0200

    Split app.js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,3 @@
-const text = document.querySelector(".title");
-const strText = text.textContent;
-const splitText = strText.split("");
-text.textContent = "";
-es = true;
-
-
-
-for(let i=0; i < splitText.length; i++){
-    text.innerHTML += "<span>" + splitText[i] + "</span>";
-}
-
-let char = 0;
-let timer = setInterval(onTick, 50);
-
-function onTick() {
-    const span = text.querySelectorAll('span')[char];
-    span.classList.add("fade");
-    char++;
-    if (char === splitText.length) {
-        complete();
-        return;
-    }
-}
-
-function complete() {
-    clearInterval(timer);
-    timer = null;
-
-}
-
-setTimeout(function(){
-window.location='cube.html';},5000);
-
-
 let scores, roundScore, activePlayer, gamePlaying, previous;
 
 const menuElement = document.getElementById('menu');
